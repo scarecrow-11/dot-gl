25c2747b8943c412ef852273fd1a1d566ee8331f
scarecrow-11 <hafizurmilon.11@gmail.com>
2022-01-17 18:48:13 +0600

    Added `mapStyle` Prop.

--- src/components/DotGL.js
+++ src/components/DotGL.js
@@ -1,29 +1,40 @@
 import React from 'react'
-// import { StaticMap } from 'react-map-gl'
-// import { DeckGL } from '@deck.gl/react'
+const PropTypes = require('prop-types')
+import { StaticMap } from 'react-map-gl'
+import { DeckGL } from '@deck.gl/react'
 
-// const INITIAL_VIEW_STATE = {
-//   longitude: -122.41669,
-//   latitude: 37.7853,
-//   zoom: 13,
-//   pitch: 0,
-//   bearing: 0
-// }
+const INITIAL_VIEW_STATE = {
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0
+}
 
 class _DotGL extends React.PureComponent {
   render() {
+    const { mapStyle } = this.props
+
     return (
-      <div>Dot GL Interactive React Map Component Library With Redux (In Development)</div>
-      // <DeckGL
-      //   initialViewState={ INITIAL_VIEW_STATE }
-      //   controller={ true }
-      // >
-      //   <StaticMap
-      //     mapStyle='https://demotiles.maplibre.org/style.json'
-      //   />
-      // </DeckGL>
+      <DeckGL
+        initialViewState={ INITIAL_VIEW_STATE }
+        controller={ true }
+      >
+        <StaticMap
+          mapStyle={ mapStyle }
+        />
+      </DeckGL>
     )
   }
+}
+
+// Prop Types
+_DotGL.propTypes = {
+  mapStyle: PropTypes.oneOfType([ PropTypes.string, PropTypes.object ])
+}
+
+_DotGL.defaultProps = {
+  mapStyle: 'https://demotiles.maplibre.org/style.json'
 }
 
 export const DotGL = _DotGL
